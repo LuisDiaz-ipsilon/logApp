33b3820d50279e657cb0831f8a1a4e338495c0bc
LuisDiaz-ipsilon <99diazluisfernand@gmail.com>
2022-06-28 19:21:12 -0500

    v4-creacion-auth-service-imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -12,7 +13,9 @@ import { RouterModule } from '@angular/router';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule
+    RouterModule,
+    HttpClientModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
